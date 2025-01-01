5b661bcabf93db468edb077ad66b3687bce6fe25
18Chinmayswar <chinmayswar18@gmail.com>
2025-01-01 20:06:18 +0530

    changes

--- JS/main.js
+++ JS/main.js
@@ -44,3 +44,51 @@ function openModal(imgElement) {
   const imageModalCarousel = new bootstrap.Modal(document.getElementById('imageModalCarousel'));
   imageModalCarousel.show();
 }
+
+
+// owl carsoule
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  nav: true,
+  dots: true,
+  margin: 40,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"],
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+    320: {
+      items: 1,
+      margin: 20
+    },
+    600: {
+      items: 2,
+      
+    },
+    960: {
+      items: 3,
+      
+    },
+    1200: {
+      items: 3,
+      
+    },
+    1024: {
+      items: 3,
+      
+    },
+    768: {
+      items: 2,
+      
+    },
+    425: {
+      items: 1,
+    }
+  }
+
+
+
+})
