12cae1a589c5358d4880412b10ed4a203b044142
18Chinmayswar <chinmayswar18@gmail.com>
2025-05-19 05:03:36 +0530

    updated

--- JS/main.js
+++ JS/main.js
@@ -7,22 +7,59 @@ let val;
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
     navbar.classList.add("header-scrolled");
-    navbar.classList.add("sticky");
+    navbar.classList.add("sticky"); 
     scrollBtn.style.display = "block";
   } else {
     navbar.classList.remove("header-scrolled");
-    navbar.classList.add("sticky");
+    navbar.classList.remove("sticky");
     scrollBtn.style.display = "none";
   }
-}
+};
+
+
+
+// window.onscroll = function () {
+//   if (document.documentElement.scrollTop > 20) {
+//     navbar.classList.add("header-scrolled");
+//     navbar.classList.add("sticky");
+//     scrollBtn.style.display = "block";
+//   } else {
+//     navbar.classList.remove("header-scrolled");
+//     navbar.classList.add("sticky");
+//     scrollBtn.style.display = "none";
+//   }
+// }
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   let navbar = document.querySelector(".navbar-m");
+//   if(!navbar) {
+//     navbar.classList.remove("header-scrolled");
+//         navbar.classList.add("sticky");
+//         scrollBtn.style.display = "none";
+
+//   } else {
+//        navbar.classList.add("header-scrolled");
+//     navbar.classList.add("sticky");
+//     scrollBtn.style.display = "block";
+
+//   }
+// });
+
+
+
+
+
+
+
 
 
 
 
 
 // nav hide small screen & open dropdown menu
-
-let navBar = document.querySelectorAll(".nav-link, .main-btn");
+let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
 
 navBar.forEach(function (a) {
@@ -37,7 +74,6 @@ navBar.forEach(function (a) {
 
 
 // onclick openModal carsoul img
-
 function openModal(imgElement) {
   const modalImageslider = document.getElementById("modalImageslider");
   modalImageslider.src = imgElement.src;  // Set modal image source to clicked image's source
@@ -47,50 +83,80 @@ function openModal(imgElement) {
 
 
 // owl carsoule
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  nav: true,
-  dots: true,
-  margin: 40,
-  navText: [
-    "<i class='fa fa-caret-left'></i>",
-    "<i class='fa fa-caret-right'></i>"],
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: true,
+    margin: 40,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"],
 
-  autoplay: true,               // Enable autoplay
-  autoplayTimeout: 3000,        // Set autoplay interval (3000ms = 3 seconds)
-  autoplayHoverPause: true,     // Pause autoplay on hover 
-  
-  responsive: {
-    0: {
-      items: 1,
-    },
-    320: {
-      items: 1,
-      margin: 20
-    },
-    600: {
-      items: 2,
-      
-    },
-    960: {
-      items: 3,
-      
-    },
-    1200: {
-      items: 3,
-      
-    },
-    1024: {
-      items: 3,
-      
-    },
-    768: {
-      items: 2,
-      
-    },
-    425: {
-      items: 1,
+    autoplay: true,               // Enable autoplay
+    autoplayTimeout: 3000,        // Set autoplay interval (3000ms = 3 seconds)
+    autoplayHoverPause: true,     // Pause autoplay on hover 
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+      320: {
+        items: 1,
+        margin: 20
+      },
+      600: {
+        items: 2,
+
+      },
+      960: {
+        items: 3,
+
+      },
+      1200: {
+        items: 3,
+
+      },
+      1024: {
+        items: 3,
+
+      },
+      768: {
+        items: 2,
+
+      },
+      425: {
+        items: 1,
+      }
     }
-  }
 
-})
+  });
+});
+
+
+
+
+// menu / header section
+window.addEventListener('load', function () {
+  fetch('../partials/header.html') // Adjust the path
+    .then(response => response.text())
+    .then(data => {
+      console.log(data)
+      document.getElementById('header_wrapper').innerHTML = data;
+    })
+    .catch(error => console.error("Error loading the header:", error));
+
+});
+
+
+// footer section
+window.addEventListener('load', function () {
+  fetch('../partials/footer.html') // Adjust the path
+    .then(response => response.text())
+    .then(data => {
+      console.log(data)
+      document.getElementById('main-footer').innerHTML = data;
+    })
+    .catch(error => console.error("Error loading the footer:", error));
+
+});
